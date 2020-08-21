fcc627f44f926f8d0cad065409d61d2c7ba723a5
AkramSheriff05 <akramsheriff05042000@gmail.com>
2020-08-21 10:39:01 -0400

    working on user post

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/spritle",{
+mongoose.connect(process.env.MONGODB||"mongodb://127.0.0.1:27017/spritle",{
     useNewUrlParser:true,
     useCreateIndex:true,
     useUnifiedTopology: true ,
